4636ec0c85cec1955b26ea6f0170f221e9b8fdcd
suchit-7x <gtsuchit7x@gmail.com>
2026-07-11 17:29:20 +0530

    air: Add flags to build with twrp minimal manifest

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -6,6 +6,12 @@
 DEVICE_PATH := device/xiaomi/air
 KERNEL_PATH := device/xiaomi/air-kernel
 
+# Allow for building with minimal manifest
+ALLOW_MISSING_DEPENDENCIES := true
+BUILD_BROKEN_DUP_RULES := true
+BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
+BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
+
 # A/B
 AB_OTA_PARTITIONS := \
     boot \
