mt6835-devs/android_device_xiaomi_air

air: Set TARGET_RECOVERY_PIXEL_FORMAT
* Recovery will self-destruct without this

Change-Id: I8c2bc981b9da92463cba2c59ee73bcbca169f556
Signed-off-by: Alvyrion <alvyrion0@gmail.com>

BoardConfig.mk
@@ -140,6 +140,7 @@ BOARD_USES_METADATA_PARTITION := true
 TARGET_BOARD_PLATFORM := mt6835
 
 # Recovery
+TARGET_RECOVERY_PIXEL_FORMAT := RGBX_8888
 TARGET_USERIMAGES_USE_F2FS := true
 
 # SPL

really-yash/twrp_device_xiaomi_air

BoardConfig.mk
@@ -127,4 +127,5 @@ TARGET_BOARD_PLATFORM := mt6835
 
 # Recovery
 TARGET_USERIMAGES_USE_F2FS := true
+TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
 BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true