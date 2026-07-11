e9ece809c3110fcd22659fd4d4e1de769bc41807
suchit-7x <gtsuchit7x@gmail.com>
2026-07-11 15:53:50 +0530

    air: Setup kernel configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -4,6 +4,7 @@
 #
 
 DEVICE_PATH := device/xiaomi/air
+KERNEL_PATH := device/xiaomi/air-kernel
 
 # Architecture
 TARGET_ARCH := arm64
@@ -15,6 +16,9 @@ TARGET_CPU_VARIANT := cortex-a76
 BOARD_BOOT_HEADER_VERSION := 4
 BOARD_USES_GENERIC_KERNEL_IMAGE := true
 BOARD_RAMDISK_USE_LZ4 := true
+BOARD_KERNEL_IMAGE_NAME := Image.gz
+BOARD_INCLUDE_DTB_IN_BOOTIMG := true
+BOARD_PREBUILT_DTBIMAGE_DIR := $(KERNEL_PATH)/dtb
 
 BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
 
@@ -32,7 +36,19 @@ BOARD_MKBOOTIMG_ARGS += \
 	--ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
 	--tags_offset $(BOARD_TAGS_OFFSET) \
 	--header_version $(BOARD_BOOT_HEADER_VERSION) \
+	--dtb $(BOARD_PREBUILT_DTBIMAGE_DIR)/mt6835.dtb \
 	--dtb_offset $(BOARD_DTB_OFFSET)
+
+# Kernel
+TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/Image.gz
+PRODUCT_COPY_FILES += \
+    $(TARGET_PREBUILT_KERNEL):kernel
+
+# Kill aosp kernel build task while preserving kernel
+TARGET_NO_KERNEL_OVERRIDE := true
+
+# Workaround to make lineage's soong generator work
+TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
 
 # Bootloader
 TARGET_BOOTLOADER_BOARD_NAME := air
