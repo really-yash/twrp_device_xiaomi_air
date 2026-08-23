83143d39614d0ed890280fee2b68c8e56626c4b7
really-yash <mail.yashraj0101@gmail.com>
2026-08-23 21:44:38 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -199,7 +199,7 @@ TWRP_INCLUDE_LOGCAT := true
 
 # OrangeFox specific variables
 OF_MAINTAINER := Yash
-OF_QUICK_BACKUP_LIST := "/data /sdcard1"
+OF_QUICK_BACKUP_LIST := /data;/sdcard1
 FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER := 1
 FOX_ASH_IS_BASH := 1
 TARGET_ARCH := arm64
