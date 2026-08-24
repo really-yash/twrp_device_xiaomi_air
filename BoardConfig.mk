d275998ab9864489e23171bf2b0925048f9d18ba
really-yash <mail.yashraj0101@gmail.com>
2026-08-24 18:22:15 +0530

    Update BoardConfig.mk

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -1,197 +1,98 @@
 #
-# SPDX-FileCopyrightText: The TeamWin Recovery Project
+# Copyright (C) 2026 The OrangeFox Recovery Project
 # SPDX-License-Identifier: Apache-2.0
 #
 
 DEVICE_PATH := device/xiaomi/air
-KERNEL_PATH := device/xiaomi/air-kernel
 
-# Allow for building with minimal manifest
+# For building with minimal manifest
 ALLOW_MISSING_DEPENDENCIES := true
 BUILD_BROKEN_DUP_RULES := true
 BUILD_BROKEN_ELF_PREBUILT_PRODUCT_COPY_FILES := true
 BUILD_BROKEN_MISSING_REQUIRED_MODULES := true
 
-# A/B
-AB_OTA_UPDATER := true
-AB_OTA_PARTITIONS := \
-    boot \
-    init_boot \
-    odm \
-    odm_dlkm \
-    product \
-    system \
-    system_ext \
-    vbmeta \
-    vbmeta_system \
-    vbmeta_vendor \
-    vendor \
-    vendor_boot \
-    vendor_dlkm
-
-# Architecture
+# --- Target Architecture & Platform ---
 TARGET_ARCH := arm64
-TARGET_ARCH_VARIANT := armv8-2a-dotprod
+TARGET_ARCH_VARIANT := armv8-a
 TARGET_CPU_ABI := arm64-v8a
-TARGET_CPU_VARIANT := cortex-a76
+TARGET_CPU_ABI2 :=
+TARGET_CPU_VARIANT := generic
+TARGET_CPU_VARIANT_RUNTIME := cortex-a55
+TARGET_BOARD_PLATFORM := mt6835
 
-# Boot image
-BOARD_BOOT_HEADER_VERSION := 4
-BOARD_USES_GENERIC_KERNEL_IMAGE := false
-BOARD_RAMDISK_USE_LZ4 := true
-BOARD_KERNEL_IMAGE_NAME := Image.gz
-BOARD_INCLUDE_DTB_IN_BOOTIMG := true
-BOARD_PREBUILT_DTBIMAGE_DIR := $(DEVICE_PATH)/dtb
+# --- 32-bit Companion Architecture (Optional Compatibility) ---
+TARGET_2ND_ARCH := arm
+TARGET_2ND_ARCH_VARIANT := armv7-a-neon
+TARGET_2ND_CPU_ABI := armeabi-v7a
+TARGET_2ND_CPU_ABI2 := armeabi
+TARGET_2ND_CPU_VARIANT := generic
 
-BOARD_KERNEL_CMDLINE += bootopt=64S3,32N2,64N2
-
-BOARD_DTB_OFFSET := 0x47c80000
-BOARD_KERNEL_BASE := 0x00000000
-BOARD_KERNEL_OFFSET := 0x40000000
-BOARD_KERNEL_PAGESIZE := 0x00001000
-BOARD_KERNEL_TAGS_OFFSET := 0x47c80000
-BOARD_RAMDISK_OFFSET := 0x66f00000
-
-BOARD_MKBOOTIMG_ARGS += \
-	--base $(BOARD_KERNEL_BASE) \
-	--pagesize $(BOARD_KERNEL_PAGESIZE) --board "" \
-	--kernel_offset $(BOARD_KERNEL_OFFSET) \
-	--ramdisk_offset $(BOARD_RAMDISK_OFFSET) \
-	--tags_offset $(BOARD_KERNEL_TAGS_OFFSET) \
-	--header_version $(BOARD_BOOT_HEADER_VERSION) \
-	--dtb $(BOARD_PREBUILT_DTBIMAGE_DIR)/mt6835.dtb \
-	--dtb_offset $(BOARD_DTB_OFFSET)
-
-# Kernel
-TARGET_PREBUILT_KERNEL := $(KERNEL_PATH)/Image.gz
-PRODUCT_COPY_FILES += \
-    $(TARGET_PREBUILT_KERNEL):kernel
-
-# Kill aosp kernel build task while preserving kernel
-TARGET_NO_KERNEL_OVERRIDE := true
-
-# Workaround to make lineage's soong generator work
-TARGET_KERNEL_SOURCE := $(KERNEL_PATH)/kernel-headers
-
-# Board Info
-TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
-
-# Bootloader
+# --- Bootloader & Target Assertions ---
 TARGET_BOOTLOADER_BOARD_NAME := air
 TARGET_NO_BOOTLOADER := true
+TARGET_OTA_ASSERT_DEVICE := air
 
-# Display
-TARGET_SCREEN_DENSITY := 320
-
-# Partitions
-BOARD_FLASH_BLOCK_SIZE := 262144
-BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_INIT_BOOT_IMAGE_PARTITION_SIZE := 8388608
-BOARD_VENDOR_BOOTIMAGE_PARTITION_SIZE := 67108864
-BOARD_SUPER_PARTITION_SIZE := 9126805504
-
-BOARD_SUPER_PARTITION_GROUPS := mediatek_dynamic_partitions
-BOARD_MEDIATEK_DYNAMIC_PARTITIONS_PARTITION_LIST += \
-    odm \
-    odm_dlkm \
-    product \
-    system \
-    system_ext \
-    vendor \
-    vendor_dlkm
-
-BOARD_MEDIATEK_DYNAMIC_PARTITIONS_SIZE := 9122611200
-
-BOARD_ODMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_ODM_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEMIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_SYSTEM_EXTIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
-BOARD_VENDOR_DLKMIMAGE_FILE_SYSTEM_TYPE := ext4
-
-TARGET_COPY_OUT_ODM := odm
-TARGET_COPY_OUT_ODM_DLKM := odm_dlkm
-TARGET_COPY_OUT_PRODUCT := product
-TARGET_COPY_OUT_SYSTEM_EXT := system_ext
-TARGET_COPY_OUT_VENDOR := vendor
-TARGET_COPY_OUT_VENDOR_DLKM := vendor_dlkm
-
-BOARD_USES_VENDOR_DLKMIMAGE := true
+# --- Recovery Boot Mode & Mounts ---
+BOARD_USES_RECOVERY_AS_BOOT := false
+TARGET_USES_MKE2FS := true
 BOARD_USES_METADATA_PARTITION := true
+BOARD_HAS_LARGE_FILESYSTEM := true
 
-# Platform & OS Version (HyperOS 2 / Android 15)
-TARGET_BOARD_PLATFORM := mt6835
+# --- Prebuilt Kernel & Device Tree Paths ---
+TARGET_PREBUILT_KERNEL := $(DEVICE_PATH)/prebuilt/vendor_boot.img-kernel
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)/prebuilt/dtbo.img
+
+# --- Boot Image Header & Kernel Parameters (MT6835 GKI 2.0) ---
+BOARD_BOOTIMG_HEADER_VERSION := 4
+BOARD_KERNEL_BASE := 0x3fff8000
+BOARD_KERNEL_CMDLINE := bootopt=64S3,32N2,64N2
+BOARD_KERNEL_PAGESIZE := 4096
+BOARD_RAMDISK_OFFSET := 0x26f08000
+BOARD_KERNEL_TAGS_OFFSET := 0x07c88000
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOTIMG_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --ramdisk_offset $(BOARD_RAMDISK_OFFSET)
+BOARD_MKBOOTIMG_ARGS += --tags_offset $(BOARD_KERNEL_TAGS_OFFSET)
+
+# --- Android 15 / HyperOS 2 Version & Platform Flags ---
 PLATFORM_VERSION := 15
 PLATFORM_SECURITY_PATCH := 2024-08-01
 BOARD_SYSTEMSDK_VERSIONS := 35
 
-# Recovery
-TARGET_USERIMAGES_USE_F2FS := true
-TARGET_RECOVERY_PIXEL_FORMAT := RGBA_8888
-BOARD_INCLUDE_RECOVERY_RAMDISK_IN_VENDOR_BOOT := true
-BOARD_MOVE_RECOVERY_RESOURCES_TO_VENDOR_BOOT := true
-BOARD_MOVE_GSI_AVB_KEYS_TO_VENDOR_BOOT := true
+# --- A/B & Virtual A/B Partition Layout ---
+AB_OTA_UPDATER := true
+BOARD_USES_AB_IMAGE := true
+BOARD_SUPER_PARTITION_BLOCK_DEVICES := super
+BOARD_SUPER_PARTITION_GROUPS := air_dynamic_partitions
 
-# SPL
-BOOT_SECURITY_PATCH := 2099-12-31
-VENDOR_SECURITY_PATCH := 2099-12-31
+# --- Dynamic Partition Sizing ---
+BOARD_SUPER_PARTITION_SIZE := 9126805504
+BOARD_AIR_DYNAMIC_PARTITIONS_PARTITION_LIST := system system_ext vendor product odm vendor_dlkm
+BOARD_AIR_DYNAMIC_PARTITIONS_SIZE := 9122611200
 
-# Verified Boot
-BOARD_AVB_ENABLE := true
-BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+# --- File System Types ---
+BOARD_SYSTEMIMAGE_PARTITION_TYPE := ext4
+BOARD_USERDATAIMAGE_FILE_SYSTEM_TYPE := f2fs
+BOARD_VENDORIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_PRODUCTIMAGE_FILE_SYSTEM_TYPE := ext4
+BOARD_ODMIIMAGE_FILE_SYSTEM_TYPE := ext4
 
-# Crypto & Decryption (FBE v2)
+# --- Encryption & Crypto (FBE v2) ---
 TW_INCLUDE_CRYPTO := true
 TW_INCLUDE_CRYPTO_FBE := true
 TW_INCLUDE_FBE_METADATA_DECRYPT := true
 TW_USE_FSCRYPT_POLICY := 2
 TW_PREPARE_DATA_MEDIA_EARLY := true
 
-# TWRP Configuration
-RECOVERY_SDCARD_ON_DATA := true
-TW_BRIGHTNESS_PATH := "/sys/class/leds/lcd-backlight/brightness"
-TW_MAX_BRIGHTNESS := 2047
-TW_DEFAULT_BRIGHTNESS := 900
-TW_INPUT_BLACKLIST := "hbtp_vm"
-TW_CUSTOM_CPU_TEMP_PATH := "/sys/class/thermal/thermal_zone27/temp"
-TW_EXCLUDE_APEX := true
-TW_BACKUP_EXCLUSIONS := /data/fonts/files
+# --- Display & UI Configuration ---
+TARGET_SCREEN_DENSITY := 320
+TW_THEME := portrait_vhd
+TW_Y_OFFSET := 0
+TW_H_OFFSET := 0
 
-# For USB OTG
-TW_EXCLUDE_DEFAULT_USB_INIT := true
-TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.usb0/lun.%d/file
-
-# Filesystem tools
-TW_INCLUDE_NTFS_3G := true
-TARGET_USES_MKE2FS := true
-
-# Display UI
-TW_NO_SCREEN_BLANK := true
-TW_INCLUDE_SCREENSHOT := true
-TW_SCREENSHOT_PATH := /sdcard/Pictures/Screenshots
-TW_THEME := portrait_hdpi
-TW_FRAMERATE := 60
-TW_STATUS_ICONS_ALIGN := center
-TW_TIME_ZONE_GUIDE := "IST-5:30"
-TW_CUSTOM_CPU_POS := 50
-TW_CUSTOM_CLOCK_POS := 300
-TW_CUSTOM_BATTERY_POS := 800
-
-# TWRP binaries tools
-TW_EXCLUDE_NANO := true
-TW_INCLUDE_LPDUMP := true
-TW_INCLUDE_LPTOOLS := true
-TW_INCLUDE_PYTHON := true
-TW_INCLUDE_RESETPROP := true
-TARGET_USES_LOGD := true
-TWRP_INCLUDE_LOGCAT := true
-
-# OrangeFox specific variables
-OF_MAINTAINER := Yash
+# --- Recovery Features & Flashing Safety ---
+TW_INCLUDE_FASTBOOTD := true
 FOX_VIRTUAL_AB_DEVICE := 1
-FOX_USE_TWRP_RECOVERY_IMAGE_BUILDER := 1
-FOX_ASH_IS_BASH := 1
-OF_USE_MAGISKBOOT := 1
-OF_FBE_METADATA_MOUNT_IGNORE := 1
+BOARD_AVB_ENABLE := true
+TW_DISABLE_ALLOW_AVB_KEY_MISMATCH := false
 TW_INCLUDE_REPACKTOOLS := true
+TW_USE_TOOLBOX := true
